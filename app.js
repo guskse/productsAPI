10bd3c29bb7947159616ae7359488fb80ab20f71
gustavo <guskse@gmail.com>
2023-09-12 15:08:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+//async errors
+
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+//middlewares
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>');
+});
+
+app.use("/api/v1/products", productsRouter);
+
+//products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    //connect to DB
+    await connectDB(process.env.MONGO_URI);
+    console.log("connected to the DB");
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
