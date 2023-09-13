1c5c3d00dfe19312ef949eeec739f4bd248b0943
gustavo <guskse@gmail.com>
2023-09-13 11:04:08 -0400

    added numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
 
   const queryObject = {};
 
@@ -22,7 +22,31 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" }; //i = case insensitive
   }
 
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
   console.log("queryObject:", queryObject);
+  
   let result = Product.find(queryObject);
 
   if (sort) {
@@ -35,8 +59,15 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldList);
   }
 
+  //pagination logic//
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+  ///////////////////////////////////////////
+
   const products = await result;
-  res.status(200).json({ nbHits: products.length, products });
+  res.status(200).json({ page, nbHits: products.length, products });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
